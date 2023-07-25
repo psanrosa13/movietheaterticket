83d08b893273b0fb6f86fb9c914d0690d04eae73
Paula Santana <paula.santana@farfetch.com>
2023-07-25 12:09:07 +0100

    Project structure

---
+++ src/main/java/com/example/movietheaterticket/MovietheaterticketApplication.java
@@ -0,0 +1,13 @@
+package com.example.movietheaterticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietheaterticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietheaterticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movietheaterticket/MovietheaterticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movietheaterticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovietheaterticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
